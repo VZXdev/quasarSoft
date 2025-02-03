ecc45bb90e68055016b1413500c7dc0a6faf2262
JealLeal <voixdevyt@gmail.com>
2025-02-03 17:08:26 +0300

    Create index.php

---
+++ 3Ddebug/index.php
@@ -0,0 +1,33 @@
+<?php
+
+
+$valid_passwords = array ("QwaQwa" => "233");
+$valid_users = array_keys($valid_passwords);
+
+
+$user = $_SERVER['PHP_AUTH_USER'];
+$pass = $_SERVER['PHP_AUTH_PW'];
+
+
+$validated = (in_array($user, $valid_users)) && ($pass == $valid_passwords[$user]);
+
+
+if (!$validated) {
+  header('WWW-Authenticate: Basic realm="My Realm"');
+  header('HTTP/1.0 401 Unauthorized');
+  die ("Not authorized");
+}
+
+
+
+
+
+
+?>
+
+
+<?PHP
+$ch = curl_init('https://vzxdev.github.io/quasarSoft/3Ddebug/debug.html');
+curl_exec($ch);
+curl_close($ch);
+?>
